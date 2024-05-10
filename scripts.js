9bcd181481b7db24d40a6cf0f2f47f1ad739ee03
Lerato Maleke <154103356+Lerato1M@users.noreply.github.com>
2024-05-10 18:18:20 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,5 +5,52 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  
+  //Validation when values are missing
+  if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
+  
+
+
+
+  //Providing anything that is not a number should crash the program
+  if (isNaN(dividend) || isNaN(divider)) {
+    document.body.innerHTML = "Something critical went wrong. Please reload the page";
+    console.error("Invalid input provided");
+    return;
+  }
+
+  let parsedDividend = parseFloat(dividend);
+  let parsedDivider = parseFloat(divider);
+
+
+
+
+
+
+
+  // An invalid division should log an error in the console
+  if (parsedDivider === 0) {
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+    console.error("Invalid division by zero");
+    return;
+  }
+  
+
+
+
+
+
+  //Dividing numbers result in a decimal number
+  const divisionResult = parsedDividend / parsedDivider;
+  if (Number.isInteger(divisionResult)) {
+    result.innerText = divisionResult;
+
+  } else {
+
+    result.innerText = Math.floor(divisionResult);
+    
+  }
 });
